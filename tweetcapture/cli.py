0e15387c159838ba07dcec7abbe8c7f9f253a60b
Xacnio <me@xacnio.dev>
2022-12-01 20:34:39 +0300

    hide medias, gui mode

--- tweetcapture/cli.py
+++ tweetcapture/cli.py
@@ -15,9 +15,18 @@ def parse_args():
     parser.add_argument('-sp', '--show-parent-tweets', dest='show_parent_tweets', action='store_true', help="Show parent tweets")
     parser.add_argument('-sm', '--show-mentions', type=int, help="Show mentions count (default: 0)", default=0)
     parser.add_argument('-r', '--radius', type=int, help="Image radius", default=30)
+
+    parser.add_argument('-hp', '--hide-photos', dest='hide_tweet_photos', action='store_true', help="Hide tweet photos")
+    parser.add_argument('-hv', '--hide-videos', dest='hide_tweet_videos', action='store_true', help="Hide tweet videos")
+    parser.add_argument('-hg', '--hide-gifs', dest='hide_tweet_gifs', action='store_true', help="Hide tweet gifs")
+    parser.add_argument('-hq', '--hide-quotes', dest='hide_tweet_quotes', action='store_true', help="Hide tweet quotes")
+    parser.add_argument('-hlp', '--hide-link-previews', dest='hide_tweet_link_previews', action='store_true', help="Hide tweet link previews")
+    parser.add_argument('-ha', '--hide-all', dest='hide_all_tweet_medias', action='store_true', help="Hide all tweet medias")
+    
     parser.add_argument('--overwrite', dest='overwrite', action='store_true', help="Overwrite output file if exists")
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', help="Debug mode")
-    parser.set_defaults(show_parent_tweets=False)
+    parser.add_argument('--gui', dest='gui', action='store_true', help="GUI mode, open browser window")
+    parser.set_defaults(show_parent_tweets=False, overwrite=False, debug=False, gui=False, hide_tweet_photos=False, hide_tweet_videos=False, hide_tweet_gifs=False, hide_tweet_quotes=False, hide_tweet_link_previews=False, hide_all_tweet_medias=False)
 
     args = parser.parse_args()
     return args
@@ -28,6 +37,11 @@ def main():
     tweet = TweetCapture(args.mode, args.night_mode, show_parent_tweets=args.show_parent_tweets, show_mentions_count=args.show_mentions, overwrite=args.overwrite, radius=args.radius)
     tweet.set_lang(args.lang)
     tweet.set_wait_time(args.t)
+    if args.hide_all_tweet_medias is True: 
+        tweet.hide_all_media()
+    else: 
+        tweet.hide_media(args.hide_tweet_link_previews, args.hide_tweet_photos, args.hide_tweet_videos, args.hide_tweet_gifs, args.hide_tweet_quotes)
+    tweet.set_gui(args.gui)
     if len(args.chromedriver) > 0:
         tweet.set_chromedriver_path(args.chromedriver)
     try:
