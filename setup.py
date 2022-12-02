1ae5b3145fcc10de37031f3b83fb9835e32e45f7
Xacnio <me@xacnio.dev>
2022-12-02 20:53:55 +0300

    0.1.7

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements():
 
 setuptools.setup(
     name="tweet-capture",
-    version="0.1.6",
+    version="0.1.7",
     author="Alperen Çetin",
     author_email="xacnio@pm.me",
     description="Take a tweet screenshot",
