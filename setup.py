adaa94ba7f708389b89f50cb6d38d6cde65eee0b
Alperen Çetin <49660313+xacnio@users.noreply.github.com>
2023-04-06 17:55:53 +0300

    0.1.8

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_requirements():
 
 setuptools.setup(
     name="tweet-capture",
-    version="0.1.7",
+    version="0.1.8",
     author="Alperen Çetin",
     author_email="xacnio@pm.me",
     description="Take a tweet screenshot",
