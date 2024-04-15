212a1edeacdc34da6e1078300bfa4239038af316
Alperen Çetin <me@xacnio.dev>
2024-04-15 03:54:27 +0300

    0.2.5 (#59)
    * show parent limit #34
    
    * 0.2.5

--- tweetcapture/cli.py
+++ tweetcapture/cli.py
@@ -13,6 +13,7 @@ def parse_args():
     parser.add_argument('--chromedriver', type=str, help="Custom chromedriver path", default="")
     parser.add_argument('-o', '--output', type=str, help="Output file name", default="")
     parser.add_argument('-sp', '--show-parent-tweets', dest='show_parent_tweets', action='store_true', help="Show parent tweets")
+    parser.add_argument('-spl', '--show-parent-limit', type=int, help="Show parent tweets limit (default: -1 = unlimited)", default=-1)
     parser.add_argument('-sm', '--show-mentions', type=int, help="Show mentions count (default: 0)", default=0)
     parser.add_argument('-r', '--radius', type=int, help="Image radius", default=15)
     parser.add_argument('-s', '--scale', type=float, help="Screenshot scale (between 0.0 and 14.0) (1.0 = original, 2.0 = 2x high) (default: 1.0)", default=1.0)
@@ -28,7 +29,7 @@ def parse_args():
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', help="Debug mode")
     parser.add_argument('--gui', dest='gui', action='store_true', help="GUI mode, open browser window")
     parser.add_argument('--cookies', type=str, help="Set cookies cookie1=value1;cookie2=value2", default="")
-    parser.set_defaults(show_parent_tweets=False, overwrite=False, debug=False, gui=False, hide_tweet_photos=False, hide_tweet_videos=False, hide_tweet_gifs=False, hide_tweet_quotes=False, hide_tweet_link_previews=False, hide_all_tweet_medias=False)
+    parser.set_defaults(show_parent_limit=-1, show_parent_tweets=False, overwrite=False, debug=False, gui=False, hide_tweet_photos=False, hide_tweet_videos=False, hide_tweet_gifs=False, hide_tweet_quotes=False, hide_tweet_link_previews=False, hide_all_tweet_medias=False)
 
     args = parser.parse_args()
     return args
@@ -36,7 +37,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-    tweet = TweetCapture(args.mode, args.night_mode, show_parent_tweets=args.show_parent_tweets, show_mentions_count=args.show_mentions, overwrite=args.overwrite, radius=args.radius, scale=args.scale)
+    tweet = TweetCapture(args.mode, args.night_mode, show_parent_tweets=args.show_parent_tweets, parent_tweets_limit=args.show_parent_limit, show_mentions_count=args.show_mentions, overwrite=args.overwrite, radius=args.radius, scale=args.scale)
     tweet.set_lang(args.lang)
     tweet.set_wait_time(args.t)
     if args.hide_all_tweet_medias is True: 
